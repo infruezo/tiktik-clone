23f459490483963d95f36f7c0916e8b17517033f
belghoul <belghoul@systemd.se>
2022-07-20 18:59:25 +0100

    ...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     >
       <div className="xl:w-[1200px] mx-auto overflow-hidden h-[100vh]">
         <Navbar />
-        <div className="flex gap-6 md:gap-20">
+        <div className="flex gap-6 md:gap-20 ">
           <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
             <Sidebar />
           </div>
